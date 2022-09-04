7d6d0d5efc179d45a2adda02a090fca911b6fcff
Al <eganalan@gmail.com>
2022-09-04 15:56:44 +0000

    Add basic layout to run.py with functions needed

--- run.py
+++ run.py
@@ -14,5 +14,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("smarticus_high_scores")
 
-data = SHEET.worksheet('HS').get_all_values()
-print(data)
+
+def main_screen():
+
+    """ Present the user with the name of the game and three options.
+    1 lets the user start the game
+    2 lets the user know how to play
+    3 lets the user view the top 10 high scores
+    """
+
+
+def play():
+    """ Start the game """
+    
+
+def get_question():
+    """ Get the question from the API """
+    
+
+def how_to():
+    """ Explains how to play the game """
+
+
+def high_scores():
+    """ Create and Show the High Scores Leaderboard """
+
+
+def check_score(score):
+    """ Check if player has made it into the Leaderboard """
+    
+    
+main_screen()
