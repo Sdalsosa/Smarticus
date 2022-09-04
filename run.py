ffd90fe21b60c2daacabed20fbfdb17dc1ecb74e
Al <eganalan@gmail.com>
2022-09-04 16:56:28 +0000

    Add landing screen and dependencies
    Add: os, time, figlet_format and colored to import list
    Add: function to give landing page with user options
    Add: clear terminal function
    Add: user input validation

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-
+import os
+import time
 import gspread
 from google.oauth2.service_account import Credentials
-
+from pyfiglet import figlet_format
+from termcolor import colored
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,7 +24,39 @@ def main_screen():
     2 lets the user know how to play
     3 lets the user view the top 10 high scores
     """
+    clear_terminal()
+    print(
+        colored(
+            figlet_format(
+                "Welcome to Smarticus", font="bulbhead", justify="center"
+            ),
+            "cyan",
+        )
+    )
+    print("\n" * 2)
+    print(colored(" " * 30 + "1 - Play Game", "yellow"))
+    print(colored(" " * 30 + "2 - How to play", "yellow"))
+    print(colored(" " * 30 + "3 - High Scores Table", "yellow"))
+    print("\n" * 2)
+    num = input(" " * 22 + "Please enter one of the three options: ").strip()
+    if num == '1':
+        play()
+    elif num == '2':
+        how_to()
+    elif num == '3':
+        high_scores()
+    else:
+        clear_terminal()
+        print('\n' * 6)
+        print(colored(figlet_format('CHOOSE  A VALID  NUMBER',
+              font="bubble", justify='center'), 'red'))
+        time.sleep(2)
+        main_screen()
 
+
+def clear_terminal():
+    """ clear terminal window """
+    os.system("cls||clear")
 
 def play():
     """ Start the game """
